b06ad23fcaf597acd22825433e7729fc1a719c46
Diptanshu Giri <dipu12455@gmail.com>
2023-02-13 09:04:56 -0700

    installed mongodb npm, then connected to mongodb, i think used dotenv package, and installed several other packages, details in package.json db uri accepted by mongodb connect function todo: implement CRUD moved app.listen to inside mongodb connect func

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,9 @@
-// there are routes in this folder, but none of the routes are been implemented, do it later.
-// add the databse object from that other file
-// then create teh ATLAS credential file, locally don't post
-// then implement CRUD for dgiri12db/recipes
-//
+//env files are containers for environment variables
+//the dotenv package via npm can help with importing those vars into source code
+//include the package then run config() init function
+require('dotenv').config({path:'/Users/diptanshugiri1/On\ This\ Mac/github/Team-Project/config.env'});
+// path needed to be defined explicitly, later during server deploymment to webhost, this path needs to be obscured somehow.
+
 const express = require('express');
 //this is where body parser is added, need to add it before CRUD functions
 const bodyParser = require('body-parser');
@@ -10,6 +11,21 @@ const app = express();
 
 // the urlencoded extracts info from the POST and sends it to the request object
 app.use(bodyParser.urlencoded({extended: true}));
+
+//mongoDB middleware
+const MongoClient = require('mongodb').MongoClient;
+const connectionString = process.env.ATLAS_URI;
+//connect to MongoClient
+var db;
+MongoClient.connect(connectionString, (err, database) => {
+	if (err) return console.log(err);
+	db = database;
+	//app.listen is now inside mongodb connect function,
+	//so that only start running server when database is connected.
+	app.listen(4000, ()=>{
+		console.log("Listening to port 4000");
+	});
+});
 
 // site for learning about routes is follows:
 // https://www.freecodecamp.org/news/building-a-simple-crud-application-with-express-and-mongodb-63f80f3eb1cd/
