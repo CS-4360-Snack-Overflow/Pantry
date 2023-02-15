dde50052f88c356730bae510e2b19057b67abf34
Diptanshu Giri <dipu12455@gmail.com>
2023-02-14 21:56:00 -0700

    condensed documentation, more info in past commits already

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,19 @@
-//env files are containers for environment variables
-//the dotenv package via npm can help with importing those vars into source code
-//include the package then run config() init function
-// require('dotenv').config({path:'/Users/diptanshugiri1/On\ This\ Mac/github/Team-Project/config.env'});
-// path needed to be defined explicitly, later during server deploymment to webhost, this path needs to be obscured somehow.
+//.env file reader package
 require('dotenv').config({path: './.env'})
+
+// import express
 const express = require('express');
-//this is where body parser is added, need to add it before CRUD functions
+
+//import body-parser
 const bodyParser = require('body-parser');
 const app = express();
 
-// the urlencoded extracts info from the POST and sends it to the request object
+//url encoded extracts info from the POST
 app.use(bodyParser.urlencoded({extended: true}));
 
+//to have relative paths instead of absolute paths
 const path = require('path');
+//give path for backend folder
 const options = {root: path.join(__dirname, "../backend")};
 
 //mongoDB middleware
@@ -22,34 +23,26 @@ const connectionString = process.env.ATLAS_URI;
 var db;
 MongoClient.connect(connectionString, (err, database) => {
 	if (err) return console.log(err);
-	db = database; // this is where we get ref to the DB
-	//app.listen is now inside mongodb connect function,
-	//so that only start running server when database is connected.
+	db = database; // get reference to the DB
 	console.log("Database connection successful");
 	database.close();
 });
 
-// site for learning about routes is follows:
-// https://www.freecodecamp.org/news/building-a-simple-crud-application-with-express-and-mongodb-63f80f3eb1cd/
-
-// this is a route in express
+//'/' route for GET
 app.get('/',(req,res) => {
-	res.sendFile('index.html', options);
+	res.sendFile('index.html', options); //options is just making relative path for us.
 });
 
-// this is also a route in express
+//'route1' route for GET
 app.get('/route1',(req,res) => {
 	res.send("Welp! Looks like you activated a route bruv!")});
 
-//the following code responds to POST from client
+// a POST route called 'routepost'
 app.post('/routepost', (req, res) => {
-	console.log(req.body);
+	console.log(req.body); //receive data from POST
 	res.sendFile('index.html', options);
-	// there is a sendFile because client freezes after sending POST, sendFile will refresh their page and also give them an empty form
 });
 
-//so now Express listens to POST and req.body gives us data
-//now what's left is send this data to MongoDB with MongoDB Querying Langauge or something
 
 //implementing POST crud to MongoDB
 
@@ -71,8 +64,8 @@ app.get('/readDB', (req, res) => {
 	}
 	);
 });
-//toArray is a function of find() inside collection, to retreive data from MongoDB
-//to use the '/readDB' route, just enter in address bar
+//toArray() function retrieves data from MongoDB.
+
 app.listen(4000, ()=>{
 	console.log("Listening to port 4000");
 });
