31074cfacec59e473279f49d7f35d09bf2bb412f
jadencua <jadencua@gmail.com>
2023-02-16 12:01:23 -0700

    Correct hover behavior, remove prices from recipe cards

--- Frontend/src/components/cards/TabCardGrid.js
+++ Frontend/src/components/cards/TabCardGrid.js
@@ -67,7 +67,7 @@ export default ({
           "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
         title: "Veg Mixer",
         content: "Tomato Salad & Carrot",
-        price: "$5.99",
+        // price: "$5.99",
         rating: "5.0",
         reviews: "87",
         url: "#"
@@ -77,7 +77,7 @@ export default ({
           "https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
         title: "Macaroni",
         content: "Cheese Pizza",
-        price: "$2.99",
+        // price: "$2.99",
         rating: "4.8",
         reviews: "32",
         url: "#"
@@ -87,7 +87,7 @@ export default ({
           "https://images.unsplash.com/photo-1476224203421-9ac39bcb3327??ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
         title: "Nelli",
         content: "Hamburger & Fries",
-        price: "$7.99",
+        // price: "$7.99",
         rating: "4.9",
         reviews: "89",
         url: "#"
@@ -97,7 +97,7 @@ export default ({
           "https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
         title: "Jalapeno Poppers",
         content: "Crispy Soyabeans",
-        price: "$8.99",
+        // price: "$8.99",
         rating: "4.6",
         reviews: "12",
         url: "#"
@@ -107,7 +107,7 @@ export default ({
           "https://images.unsplash.com/photo-1473093226795-af9932fe5856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
         title: "Cajun Chicken",
         content: "Roasted Chicken & Egg",
-        price: "$7.99",
+        // price: "$7.99",
         rating: "4.2",
         reviews: "19",
         url: "#"
@@ -117,7 +117,7 @@ export default ({
           "https://images.unsplash.com/photo-1550461716-dbf266b2a8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
         title: "Chillie Cake",
         content: "Deepfried Chicken",
-        price: "$2.99",
+        // price: "$2.99",
         rating: "5.0",
         reviews: "61",
         url: "#"
@@ -127,7 +127,7 @@ export default ({
           "https://images.unsplash.com/photo-1534422298391-e4f8c172dddb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
         title: "Guacamole Mex",
         content: "Mexican Chilli",
-        price: "$3.99",
+        // price: "$3.99",
         rating: "4.2",
         reviews: "95",
         url: "#"
@@ -137,7 +137,7 @@ export default ({
           "https://images.unsplash.com/photo-1565310022184-f23a884f29da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
         title: "Carnet Nachos",
         content: "Chilli Crispy Nachos",
-        price: "$3.99",
+        // price: "$3.99",
         rating: "3.9",
         reviews: "26",
         url: "#"
@@ -213,7 +213,7 @@ export default ({
                       }}
                       transition={{ duration: 0.3 }}
                     >
-                      <CardButton>Buy Now</CardButton>
+                      <CardButton>See Recipe</CardButton>
                     </CardHoverOverlay>
                   </CardImageContainer>
                   <CardText>
