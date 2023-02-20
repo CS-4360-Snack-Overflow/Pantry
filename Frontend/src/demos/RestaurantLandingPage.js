5f927c1fb928ced76f4cdb92bee6e968acc929e6
jadencua <jadencua@gmail.com>
2023-02-20 10:19:37 -0700

    Swapped old Treact logo with new Pantry logo

--- Frontend/src/demos/RestaurantLandingPage.js
+++ Frontend/src/demos/RestaurantLandingPage.js
@@ -8,7 +8,7 @@ import MainFeature from "components/features/TwoColWithButton.js";
 import MainFeature2 from "components/features/TwoColSingleFeatureWithStats2.js";
 import TabGrid from "components/cards/TabCardGrid.js";
 import Testimonial from "components/testimonials/ThreeColumnWithProfileImage.js";
-import DownloadApp from "components/cta/DownloadApp.js";
+//import DownloadApp from "components/cta/DownloadApp.js";
 import Footer from "components/footers/FiveColumnWithInputForm.js";
 
 import chefIconImageSrc from "images/chef-icon.svg";
@@ -18,7 +18,7 @@ import shopIconImageSrc from "images/shop-icon.svg";
 export default () => {
   const Subheading = tw.span`tracking-wider text-sm font-medium`;
   const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
-  const HighlightedTextInverse = tw.span`bg-gray-100 text-primary-500 px-4 transform -skew-x-12 inline-block`;
+  //  const HighlightedTextInverse = tw.span`bg-gray-100 text-primary-500 px-4 transform -skew-x-12 inline-block`;
   const Description = tw.span`inline-block mt-8`;
   const imageCss = tw`rounded-4xl`;
   return (
@@ -127,9 +127,9 @@ export default () => {
         subheading=""
         heading={<>Novice chefs <HighlightedText>Love Us.</HighlightedText></>}
       />
-      <DownloadApp
+      {/* <DownloadApp
         text={<>People around you are cooking delicious meals using the <HighlightedTextInverse>Pantry App.</HighlightedTextInverse></>}
-      />
+      /> */}
       <Footer />
     </AnimationRevealPage>
   );
--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -5,7 +5,7 @@ import tw from "twin.macro";
 import styled from "styled-components";
 import {css} from "styled-components/macro"; //eslint-disable-line
 import illustration from "images/login-illustration.svg";
-import logo from "images/logo.svg";
+import logo from "images/logo-p.svg";
 import googleIconImageSrc from "images/google-icon.png";
 import twitterIconImageSrc from "images/twitter-icon.png";
 import { ReactComponent as LoginIcon } from "feather-icons/dist/icons/log-in.svg";
@@ -54,7 +54,7 @@ const IllustrationImage = styled.div`
 `;
 
 export default ({
-  logoLinkUrl = "#",
+  logoLinkUrl = "/",
   illustrationImageSrc = illustration,
   headingText = "Sign In To Treact",
   socialButtons = [
