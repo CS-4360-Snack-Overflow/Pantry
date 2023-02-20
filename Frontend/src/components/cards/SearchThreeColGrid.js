387616e54b66013908318ce6d9156735de950f6c
Ryan Baertlein <72224614+rdaniel58@users.noreply.github.com>
2023-02-20 15:30:55 -0700

    Began work on Recipe Page

---
+++ Frontend/src/components/cards/SearchThreeColGrid.js
@@ -0,0 +1,210 @@
+import React from "react";
+import tw from "twin.macro";
+import styled from "styled-components";
+import { css } from "styled-components/macro";
+import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
+import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings";
+import {SectionDescription} from "components/misc/Typography";
+//import { ReactComponent as TwitterIcon} from "images/twitter-icon.svg";
+import { ReactComponent as GoogleIcon} from "images/google-icon.svg";
+
+const HeadingContainer = tw.div``
+const Heading = tw(SectionHeading)``
+const Subheading = tw(SubheadingBase)`text-center mb-3`
+const Description = tw(SectionDescription)`mx-auto text-center`
+
+const Cards = tw.div`flex flex-wrap flex-row justify-center sm:max-w-2xl lg:max-w-5xl mx-auto`
+const Card = tw.div`mt-24 w-full sm:w-1/2 lg:w-1/3 flex flex-col items-center`
+const CardImage = styled.div`
+  ${props => css`background-image: url("${props.imageSrc}");`}
+  ${tw`w-64 h-64 bg-contain bg-center rounded`}
+`
+const CardContent = styled.div`
+  ${tw`flex flex-col items-center mt-6`}
+  .position {
+    ${tw`uppercase font-bold tracking-widest text-xs text-primary-500`}
+  }
+  .name {
+    ${tw`mt-1 text-xl font-medium text-gray-900`}
+  9
+`
+
+const CardLinks = styled.div`
+  ${tw`mt-6 flex`}
+  .link {
+    ${tw`mr-8 last:mr-0 text-gray-400 hocus:text-primary-500 transition duration-300`}
+    .icon {
+      ${tw`fill-current w-6 h-6`}
+    }
+  }
+`
+
+export default ({
+  heading = "Find Your Recipe Here!",
+  subheading = "Search from hundreds of recipes",
+  description = " ",
+  cards = [
+    {
+      imageSrc: "https://www.allrecipes.com/thmb/GDQxkynr_6CJ1NEu0F-ldsE-1Gg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/14385-pasta-salad-mfs-61-0d454fc38db04fd2b741b1730e5a8065.jpg",
+      position: "Tomato and Pasta Salad",
+      name: "Pasta Salad",
+      links: [
+        {
+          url: "https://www.allrecipes.com/recipe/14385/pasta-salad/",
+          icon: GoogleIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://www.allrecipes.com/thmb/GDQxkynr_6CJ1NEu0F-ldsE-1Gg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/14385-pasta-salad-mfs-61-0d454fc38db04fd2b741b1730e5a8065.jpg",
+      position: "Tomato and Pasta Salad",
+      name: "Pasta Salad",
+      links: [
+        {
+          url: "https://www.allrecipes.com/recipe/14385/pasta-salad/",
+          icon: GoogleIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://www.allrecipes.com/thmb/GDQxkynr_6CJ1NEu0F-ldsE-1Gg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/14385-pasta-salad-mfs-61-0d454fc38db04fd2b741b1730e5a8065.jpg",
+      position: "Tomato and Pasta Salad",
+      name: "Pasta Salad",
+      links: [
+        {
+          url: "https://www.allrecipes.com/recipe/14385/pasta-salad/",
+          icon: GoogleIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://www.allrecipes.com/thmb/GDQxkynr_6CJ1NEu0F-ldsE-1Gg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/14385-pasta-salad-mfs-61-0d454fc38db04fd2b741b1730e5a8065.jpg",
+      position: "Tomato and Pasta Salad",
+      name: "Pasta Salad",
+      links: [
+        {
+          url: "https://www.allrecipes.com/recipe/14385/pasta-salad/",
+          icon: GoogleIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://www.allrecipes.com/thmb/GDQxkynr_6CJ1NEu0F-ldsE-1Gg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/14385-pasta-salad-mfs-61-0d454fc38db04fd2b741b1730e5a8065.jpg",
+      position: "Tomato and Pasta Salad",
+      name: "Pasta Salad",
+      links: [
+        {
+          url: "https://www.allrecipes.com/recipe/14385/pasta-salad/",
+          icon: GoogleIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://www.allrecipes.com/thmb/GDQxkynr_6CJ1NEu0F-ldsE-1Gg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/14385-pasta-salad-mfs-61-0d454fc38db04fd2b741b1730e5a8065.jpg",
+      position: "Tomato and Pasta Salad",
+      name: "Pasta Salad",
+      links: [
+        {
+          url: "https://www.allrecipes.com/recipe/14385/pasta-salad/",
+          icon: GoogleIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://www.allrecipes.com/thmb/GDQxkynr_6CJ1NEu0F-ldsE-1Gg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/14385-pasta-salad-mfs-61-0d454fc38db04fd2b741b1730e5a8065.jpg",
+      position: "Tomato and Pasta Salad",
+      name: "Pasta Salad",
+      links: [
+        {
+          url: "https://www.allrecipes.com/recipe/14385/pasta-salad/",
+          icon: GoogleIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://www.allrecipes.com/thmb/GDQxkynr_6CJ1NEu0F-ldsE-1Gg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/14385-pasta-salad-mfs-61-0d454fc38db04fd2b741b1730e5a8065.jpg",
+      position: "Tomato and Pasta Salad",
+      name: "Pasta Salad",
+      links: [
+        {
+          url: "https://www.allrecipes.com/recipe/14385/pasta-salad/",
+          icon: GoogleIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://www.allrecipes.com/thmb/GDQxkynr_6CJ1NEu0F-ldsE-1Gg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/14385-pasta-salad-mfs-61-0d454fc38db04fd2b741b1730e5a8065.jpg",
+      position: "Tomato and Pasta Salad",
+      name: "Pasta Salad",
+      links: [
+        {
+          url: "https://www.allrecipes.com/recipe/14385/pasta-salad/",
+          icon: GoogleIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://www.allrecipes.com/thmb/GDQxkynr_6CJ1NEu0F-ldsE-1Gg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/14385-pasta-salad-mfs-61-0d454fc38db04fd2b741b1730e5a8065.jpg",
+      position: "Tomato and Pasta Salad",
+      name: "Pasta Salad",
+      links: [
+        {
+          url: "https://www.allrecipes.com/recipe/14385/pasta-salad/",
+          icon: GoogleIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://www.allrecipes.com/thmb/GDQxkynr_6CJ1NEu0F-ldsE-1Gg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/14385-pasta-salad-mfs-61-0d454fc38db04fd2b741b1730e5a8065.jpg",
+      position: "Tomato and Pasta Salad",
+      name: "Pasta Salad",
+      links: [
+        {
+          url: "https://www.allrecipes.com/recipe/14385/pasta-salad/",
+          icon: GoogleIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://www.allrecipes.com/thmb/GDQxkynr_6CJ1NEu0F-ldsE-1Gg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/14385-pasta-salad-mfs-61-0d454fc38db04fd2b741b1730e5a8065.jpg",
+      position: "Tomato and Pasta Salad",
+      name: "Pasta Salad",
+      links: [
+        {
+          url: "https://www.allrecipes.com/recipe/14385/pasta-salad/",
+          icon: GoogleIcon,
+        },
+      ],
+    },
+  ]
+}) => {
+  return (
+    <Container>
+      <ContentWithPaddingXl>
+        <HeadingContainer>
+          {subheading && <Subheading>{subheading}</Subheading>}
+          {heading && <Heading>{heading}</Heading> }
+          {description && <Description>{description}</Description> }
+        </HeadingContainer>
+        <Cards>
+          {cards.map((card, index) => (
+            <Card key={index}>
+              <CardImage imageSrc={card.imageSrc} />
+              <CardContent>
+                <span className="position">{card.position}</span>
+                <span className="name">{card.name}</span>
+                <CardLinks>
+                  {card.links.map((link, linkIndex) => (
+                    <a key={linkIndex} className="link" href={link.url}>
+                      <link.icon className="icon" />
+                    </a>
+                  ))}
+                </CardLinks>
+              </CardContent>
+            </Card>
+          ))}
+        </Cards>
+      </ContentWithPaddingXl>
+    </Container>
+  );
+};
--- Frontend/src/demos/ServiceLandingPage.js
+++ Frontend/src/demos/ServiceLandingPage.js
@@ -1,48 +1,19 @@
 import React from "react";
-//import tw from "twin.macro";
-import { css } from "styled-components/macro"; //eslint-disable-line
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
-
-
-import Hero from "components/hero/TwoColumnWithFeaturesAndTestimonial.js";
-//import Features from "components/features/ThreeColWithSideImage.js";
-//import MainFeature from "components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
-//import FeatureStats from "components/features/ThreeColCenteredStatsPrimaryBackground.js";
-//import Pricing from "components/pricing/TwoPlansWithDurationSwitcher.js";
-//import Blog from "components/blogs/GridWithFeaturedPost.js";
-//import Testimonial from "components/testimonials/TwoColumnWithImageAndRating.js";
-//import FAQ from "components/faqs/SingleCol.js";
-//import GetStarted from "components/cta/GetStartedLight.js";
+import tw from "twin.macro";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import Header from "components/headers/light.js";
 import Footer from "components/footers/FiveColumnWithInputForm.js";
-
-//const HighlightedText = tw.span`text-primary-500`
+import RecipeSearchBar from "components/forms/SearchBarWithIllustration";
+import SearchCardGrid from "components/cards/SearchThreeColGrid.js";
 
 export default () => {
   return (
     <AnimationRevealPage>
-      <Hero />
-      {/* <FeatureStats/>
-      <Features
-        heading={<>Amazing <HighlightedText>Features</HighlightedText></>}
-      />
-      <MainFeature
-        heading={<>Cloud built by and for <HighlightedText>Professionals</HighlightedText></>}
-      />
-      <Testimonial
-        heading={<>Our Clients <HighlightedText>Love Us</HighlightedText></>}
-      />
-      <Pricing
-        heading={<>Flexible <HighlightedText>Plans</HighlightedText></>}
-      />
-      <FAQ
-        heading={<>Any <HighlightedText>Questions ?</HighlightedText></>}
-      />
-      <Blog
-        subheading="Blog"
-        heading={<>We love <HighlightedText>Writing</HighlightedText></>}
-      />
-      <GetStarted/> */}
+      <Header />
+      <RecipeSearchBar />
+      <SearchCardGrid />
       <Footer />
     </AnimationRevealPage>
   );
-}
+};
