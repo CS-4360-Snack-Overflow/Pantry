1cbfde5c898abf13814eb8e6c1f27830dc2fa218
jadencua <jadencua@gmail.com>
2023-02-21 09:39:58 -0700

    Replaced old footer on all pages with new footer

--- Frontend/src/pages/ContactUs.js
+++ Frontend/src/pages/ContactUs.js
@@ -3,7 +3,7 @@ import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import tw from "twin.macro";
 import { css } from "styled-components/macro"; //eslint-disable-line
 import Header from "components/headers/light.js";
-import Footer from "components/footers/FiveColumnWithInputForm.js";
+import Footer from "components/footers/MiniCenteredFooter";
 import ContactUsForm from "components/forms/TwoColContactUsWithIllustrationFullForm.js";
 import ContactDetails from "components/cards/ThreeColContactDetails.js";
 
