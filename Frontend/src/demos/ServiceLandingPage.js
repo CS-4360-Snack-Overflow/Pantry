327333aefeb5afe100064362f8cd08eb06e81c4e
Ryan Baertlein <rdaniel707@yahoo.com>
2023-02-22 17:39:17 -0700

    Changed recipe grid to be a tab card.

--- Frontend/src/demos/ServiceLandingPage.js
+++ Frontend/src/demos/ServiceLandingPage.js
@@ -6,13 +6,14 @@ import Header from "components/headers/light.js";
 import Footer from "components/footers/MiniCenteredFooter";
 import RecipeSearchBar from "components/forms/SearchBarWithIllustration";
 import SearchCardGrid from "components/cards/SearchThreeColGrid.js";
+import RecipeGrid from "components/cards/TabCardRecipeGrid.js";
 
 export default () => {
   return (
     <AnimationRevealPage>
       <Header />
-      <RecipeSearchBar />
-      <SearchCardGrid />
+
+      <RecipeGrid />
       <Footer />
     </AnimationRevealPage>
   );
