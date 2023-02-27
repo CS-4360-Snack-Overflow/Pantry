467772d3c54f30b8be5510078d8e05ebf0fbe67b
Diptanshu Giri <dipu12455@gmail.com>
2023-02-27 14:41:25 -0700

    finished up with update and create as same route. searches for users displays list of users in the html page TODO: messages sent back by server is json format, needs a frontend to make sense of those messages TODO: need to add user delete function using their username Future TODO: ask for user password to authenticate

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -4,31 +4,48 @@ const User = require('../models/user');
 
 // Create a new user
 router.post('/userCreate', async (req, res) => {
-  try {
-    const user = new User({
-      fullName: req.body.fullName,
-      emailAddress: req.body.emailAddress,
-      username: req.body.username,
-      password: req.body.password,
-      dateOfBirth: req.body.dateOfBirth,
-      gender: req.body.gender,
-      countryRegion: req.body.countryRegion
-    });
-    const savedUser = await user.save();
-    res.status(201).json(savedUser);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+	try {
+		const user = new User({
+			fullName: req.body.fullName,
+			emailAddress: req.body.emailAddress,
+			username: req.body.username,
+			password: req.body.password,
+			dateOfBirth: req.body.dateOfBirth,
+			gender: req.body.gender,
+			countryRegion: req.body.countryRegion
+		});
+		// check here if the above username exists, if so only update, don't create new entry
+
+		// first access the username attribute from the model
+		const username = user.username;
+		console.log(`user.username=${username}`);
+		const updates = req.body;
+
+		const updatedUser = await User.findOneAndUpdate({ username }, updates, { new: true});
+
+		// if update was successful, there's that
+		// if not, create new user instead
+		if (updatedUser) {
+			res.json(updatedUser);
+		} else {
+			const savedUser = await user.save();
+			res.status(201).json(savedUser);
+		}
+
+	} catch (err) {
+		res.status(400).json({ message: err.message });
+	}
 });
 
 // Get all users
 router.get('/userRead', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+	console.log("message: Accessed userRead route");
+	try {
+		const users = await User.find();
+		res.json(users);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 });
 
 // user search route
@@ -45,24 +62,7 @@ router.get('/userSearch', async (req, res) => {
 	}
 });
 
-//user update
-router.put('/userUpdate', async (req, res) => {
-	try {
-		const username = req.query.username;
-		const updates = req.body;
-
-		const updatedUser = await User.findOneAndUpdate({ username }, updates, { new: true
-		});
-
-		if (updatedUser) {
-			res.json(updatesUser);
-		} else {
-			res.status(404).json({ message: 'User not found' });
-		}
-	} catch (err) {
-		res.status(500).json({ message: err.message });
-	}
-});
+// delete function for user remains
 
 module.exports = router;
 
