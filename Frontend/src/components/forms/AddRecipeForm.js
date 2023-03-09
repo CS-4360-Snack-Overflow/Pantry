ee20c878dccb61808ae93da7a8fa3f03c5c4a656
jadencua <jadencua@gmail.com>
2023-03-09 16:03:16 -0700

    testing React/Tailwind options

--- Frontend/src/components/forms/AddRecipeForm.js
+++ Frontend/src/components/forms/AddRecipeForm.js
@@ -5,7 +5,7 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings.js";
 import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons.js";
 //import EmailIllustrationSrc from "images/email-illustration.svg";
-
+ 
 const Container = tw.div`relative`;
 const TwoColumn = tw.div`flex flex-col md:flex-row justify-between max-w-screen-xl mx-auto py-20 md:py-24`;
 const Column = tw.div`w-full max-w-md mx-auto md:max-w-none md:mx-0`;
@@ -20,14 +20,11 @@ const TextColumn = styled(Column)(props => [
 //   tw`rounded bg-contain bg-no-repeat bg-center h-full`,
 // ]);
 const TextContent = tw.div`lg:py-8 text-center md:text-left`;
-
 const Subheading = tw(SubheadingBase)`text-center md:text-left`;
 const Heading = tw(SectionHeading)`mt-4 font-black text-left text-3xl sm:text-4xl lg:text-5xl text-center md:text-left leading-tight`;
 const Description = tw.p`mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100`
-
 const Form = tw.form`mt-8 md:mt-10 text-sm flex flex-col lg:flex-row`
 const Input = tw.input`border-2 px-5 py-3 rounded focus:outline-none font-medium transition duration-300 hocus:border-primary-500`
-
 const SubmitButton = tw(PrimaryButtonBase)`inline-block lg:ml-6 mt-6 lg:mt-0`
 
 export default ({
@@ -47,7 +44,7 @@ export default ({
         {/* <ImageColumn>
           <Image imageSrc={EmailIllustrationSrc} />
         </ImageColumn> */}
-        <TextColumn textOnLeft={textOnLeft}>
+        {/* <TextColumn textOnLeft={textOnLeft}>
           <TextContent>
             {subheading && <Subheading>{subheading}</Subheading>}
             <Heading>{heading}</Heading>
@@ -56,8 +53,18 @@ export default ({
               <Input type="email" name="email" placeholder="Placeholder text" />
               <SubmitButton type="submit">{submitButtonText}</SubmitButton>
             </Form>
-          </TextContent>
-        </TextColumn>
+          </TextContent> */}
+
+<TextContent>
+<Heading>{heading}</Heading>
+            <Description>{description}</Description>
+            <Form action={formAction} method={formMethod}>
+              <Input type="text" name="recipeInstructions" multiline={true} placeholder="Placeholder text" />
+              <SubmitButton type="submit">{submitButtonText}</SubmitButton>
+            </Form>
+</TextContent>
+
+
       </TwoColumn>
     </Container>
   );
