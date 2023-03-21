ddc5e623175962e46fd6da160c9c81026092af53
jadencua <jadencua@gmail.com>
2023-03-21 09:36:16 -0600

    Added navbar to userprofile page and also added animation container so that animations can be used later

--- Frontend/src/pages/User.js
+++ Frontend/src/pages/User.js
@@ -8,15 +8,20 @@ import { SectionHeading } from "components/misc/Headings.js";
 import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons.js";
 import { ReactComponent as SvgDecoratorBlob1 } from "images/svg-decorator-blob-5.svg";
 import { ReactComponent as SvgDecoratorBlob2 } from "images/svg-decorator-blob-7.svg";
+import Header from "components/headers/light.js";
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 //import RecipeSearchBar from "components/forms/SearchBarWithIllustration";
 
 const HeaderRow = tw.div`flex justify-between items-center flex-col xl:flex-row`;
-const Header = tw(SectionHeading)``;
-
-
+const H1 = tw(SectionHeading)``;
 const TabContent = tw(motion.div)`mt-6 flex flex-wrap sm:-mr-10 md:-mr-6 lg:-mr-12`;
 const CardContainer = tw.div`mt-10 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 sm:pr-10 md:pr-6 lg:pr-12`;
 const Card = tw(motion.a)`bg-gray-200 rounded-b block max-w-xs mx-auto sm:max-w-none sm:mx-0`;
+const CardText = tw.div`p-4 text-gray-900`;
+const CardTitle = tw.h5`text-lg font-semibold group-hover:text-primary-500`;
+const CardContent = tw.p`mt-1 text-sm font-medium text-gray-600`;
+const CardPrice = tw.p`mt-4 text-xl font-bold`;
+const CardButton = tw(PrimaryButtonBase)`text-sm`;
 const CardImageContainer = styled.div`
   ${props => css`background-image: url("${props.imageSrc}");`}
   ${tw`h-56 xl:h-64 bg-center bg-cover relative rounded-t`}
@@ -26,14 +31,6 @@ const CardHoverOverlay = styled(motion.div)`
   background-color: rgba(255, 255, 255, 0.5);
   ${tw`absolute inset-0 flex justify-center items-center`}
 `;
-const CardButton = tw(PrimaryButtonBase)`text-sm`;
-
-
-
-const CardText = tw.div`p-4 text-gray-900`;
-const CardTitle = tw.h5`text-lg font-semibold group-hover:text-primary-500`;
-const CardContent = tw.p`mt-1 text-sm font-medium text-gray-600`;
-const CardPrice = tw.p`mt-4 text-xl font-bold`;
 
 const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
   ${tw`pointer-events-none -z-20 absolute right-0 top-0 h-64 w-64 opacity-15 transform translate-x-2/3 -translate-y-12 text-pink-400`}
@@ -42,13 +39,8 @@ const DecoratorBlob2 = styled(SvgDecoratorBlob2)`
   ${tw`pointer-events-none -z-20 absolute left-0 bottom-0 h-80 w-80 opacity-15 transform -translate-x-2/3 text-primary-500`}
 `;
 
-
-
-
-
 export default ({
   heading = "Your Favorites",
-  
   tabs = {
     Tabs: [
       {
@@ -86,69 +78,68 @@ export default ({
 
   //<RecipeSearchBar/> added after context with padding below
   return (
-    <Container>
-      <ContentWithPaddingXl>
-        
-      
-        <HeaderRow>
-          <Header>{heading}</Header>
-          
-        </HeaderRow>
-
-        {tabsKeys.map((tabKey, index) => (
-          <TabContent
-            key={index}
-            variants={{
-              current: {
-                opacity: 1,
-                scale:1,
-                display: "flex",
-              },
-              hidden: {
-                opacity: 0,
-                scale:0.8,
-                display: "none",
-              }
-            }}
-            transition={{ duration: 0.4 }}
-            initial={activeTab === tabKey ? "current" : "hidden"}
-            animate={activeTab === tabKey ? "current" : "hidden"}
-          >
-            {tabs[tabKey].map((card, index) => (
-              <CardContainer key={index}>
-                <Card className="group" href={card.url} initial="rest" whileHover="hover" animate="rest">
-                  <CardImageContainer imageSrc={card.imageSrc}>
-                    
-                    <CardHoverOverlay
-                      variants={{
-                        hover: {
-                          opacity: 1,
-                          height: "auto"
-                        },
-                        rest: {
-                          opacity: 0,
-                          height: 0
-                        }
-                      }}
-                      transition={{ duration: 0.3 }}
-                    >
-                      <CardButton>Search</CardButton>
-                    </CardHoverOverlay>
-                  </CardImageContainer>
-                  <CardText>
-                    <CardTitle>{card.title}</CardTitle>
-                    <CardContent>{card.content}</CardContent>
-                    <CardPrice>{card.price}</CardPrice>
-                  </CardText>
-                </Card>
-              </CardContainer>
-            ))}
-          </TabContent>
+    <AnimationRevealPage>
+      <Container>
+        <Header />
+        <ContentWithPaddingXl>
+          <HeaderRow>
+            <H1>{heading}</H1>
+          </HeaderRow>
+          {tabsKeys.map((tabKey, index) => (
+            <TabContent
+              key={index}
+              variants={{
+                current: {
+                  opacity: 1,
+                  scale:1,
+                  display: "flex",
+                },
+                hidden: {
+                  opacity: 0,
+                  scale:0.8,
+                  display: "none",
+                }
+              }}
+              transition={{ duration: 0.4 }}
+              initial={activeTab === tabKey ? "current" : "hidden"}
+              animate={activeTab === tabKey ? "current" : "hidden"}>
+                {tabs[tabKey].map((card, index) => (
+                  <CardContainer key={index}>
+                    <Card className="group" href={card.url} initial="rest" whileHover="hover" animate="rest">
+                      <CardImageContainer imageSrc={card.imageSrc}>
+                        
+                        <CardHoverOverlay
+                          variants={{
+                            hover: {
+                              opacity: 1,
+                              height: "auto"
+                            },
+                            rest: {
+                              opacity: 0,
+                              height: 0
+                            }
+                          }}
+                          transition={{ duration: 0.3 }}
+                        >
+                          <CardButton>Search</CardButton>
+                        </CardHoverOverlay>
+                      </CardImageContainer>
+                      <CardText>
+                        <CardTitle>{card.title}</CardTitle>
+                        <CardContent>{card.content}</CardContent>
+                        <CardPrice>{card.price}</CardPrice>
+                      </CardText>
+                    </Card>
+                  </CardContainer>
+                ))}
+              </TabContent>
         ))}
-      </ContentWithPaddingXl>
-      <DecoratorBlob1 />
-      <DecoratorBlob2 />
-    </Container>
+        </ContentWithPaddingXl>
+        <DecoratorBlob1 />
+        <DecoratorBlob2 />
+      </Container>
+    </AnimationRevealPage>
+    
   );
 };
 
