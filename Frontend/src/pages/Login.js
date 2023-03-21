5b283b1106ba32e016d805b22eeecdb3553c3554
jadencua <jadencua@gmail.com>
2023-03-21 10:04:53 -0600

    Removed unused code and replaced image on signup page

--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -20,7 +20,6 @@ const Form = tw.form`mx-auto max-w-xs`;
 const Input = tw.input`w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none 
   focus:border-gray-400 focus:bg-white mt-5 first:mt-0`;
 const IllustrationContainer = tw.div`sm:rounded-r-lg flex-1 bg-purple-100 text-center hidden lg:flex justify-center`;
-
 const SubmitButton = styled.button`
   ${tw`mt-5 tracking-wide font-semibold bg-primary-500 text-gray-100 w-full py-4 rounded-lg hover:bg-primary-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none`}
   .icon {
