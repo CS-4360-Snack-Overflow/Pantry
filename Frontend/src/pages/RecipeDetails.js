a327c439daa86a43e555ecc47aad23c1f8dffcae
Wolfy Fiorini <jonwolffiorini@gmail.com>
2023-03-31 16:38:11 -0600

    cleanup

--- Frontend/src/pages/RecipeDetails.js
+++ Frontend/src/pages/RecipeDetails.js
@@ -1,15 +1,12 @@
 import React from "react";
 import tw from "twin.macro";
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
-//import styled from "styled-components";
-import { css } from "styled-components/macro"; //eslint-disable-line
+import styled from "styled-components";
 import Header from "components/headers/light.js";
 import Footer from "components/footers/MiniCenteredFooter";
-import AddRecipeForm from "components/forms/AddRecipeForm";
 import { useLocation } from "react-router-dom";
 
 const Heading = tw.h2`text-4xl sm:text-5xl font-black tracking-wide text-center pt-10 md:pt-24`;
-const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
 
 //-----------------RECIPE FIELDS TO FILL IN------------------
 // Recipe Header
@@ -40,9 +37,7 @@ const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -ske
   //    recipe.nutrition.fiber - Number (grams)
 
 export default () => {
-  const state = useLocation().state;
-  console.log(state)
-  const recipe = state.clickedRecipe
+  const recipe = useLocation().state.clickedRecipe
   return (
     <AnimationRevealPage>
       <Header />
